ae7741f80ee61bd660fb3ae160e1056e2f78e6e2
Davi Kawasaki <davishinjik@gmail.com>
2018-02-03 01:27:11 -0200

    Express config - static, bodyParser, routes injection, error handler

---
+++ config/express.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const consign = require('consign');
+const bodyParser = require('body-parser');
+
+module.exports = () => {
+    const app = express();
+
+    app.use(express.static('./app/public'));
+    app.set('view engine', 'ejs');
+    app.set('views', './app/view');
+
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+
+    consign({cwd: 'app'})
+        .include('route')
+        .into(app);
+    
+    app.get('/', function(req, res, next) {
+        res.redirect('/orders');
+    });
+
+    app.use((err, req, res, next) => {
+        if(process.env.NODE_ENV == 'production') {
+            if(err) {
+                console.error(err);
+                res.status(500).render('errors/500');
+                return;
+            }
+        }
+        console.error(err);
+        next(err);
+    });
+
+    return app;
+}
